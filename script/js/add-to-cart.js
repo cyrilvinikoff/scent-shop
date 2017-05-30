c21ca02185cb9ebc4b47ea46d5e703c9779e78bf
Cyril Vinikoff <cyril.vinikoff@hetic.net>
2017-05-30 12:01:09 +0200

    content

--- script/js/add-to-cart.js
+++ script/js/add-to-cart.js
@@ -3,8 +3,15 @@ var shop = document.querySelector('.shop');
 
 shop.addEventListener('click', function() {
   var nbItems = localStorage.getItem('count');
+  if (nbItems != null) {
+    nbItems = parseInt(nbItems);
+  }
+  else {
+    nbItems = 0;
+  }
+
   nbItems++;
-  
+
   localStorage.setItem('count', nbItems);
   number.textContent = nbItems;
 });
--- script/js/save-storage.js
+++ script/js/save-storage.js
@@ -1,5 +1,12 @@
 var number = document.querySelector('nav ul li span');
 
 var nbItems = localStorage.getItem('count');
+if (nbItems != null) {
+  nbItems = parseInt(nbItems);
+}
+else {
+  nbItems = 0;
+}
+
 localStorage.setItem('count', nbItems);
 number.textContent = nbItems;
