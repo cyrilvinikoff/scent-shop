988c57d600a3f787d0ecb52fe21fcd166bbfa689
Cyril Vinikoff <cyril.vinikoff@hetic.net>
2017-05-31 20:49:57 +0200

    basket modal

---
+++ script/js/show-basket.js
@@ -0,0 +1,21 @@
+var basket = document.querySelector('.basket');
+var basketDetails = document.querySelector('.basket-details');
+
+if (window.innerWidth > 900) {
+  basket.addEventListener('mouseover', function() {
+    basketDetails.style.opacity = 1;
+    basketDetails.style.display = 'block';
+  });
+  basketDetails.addEventListener('mouseover', function() {
+    basketDetails.style.opacity = 1;
+    basketDetails.style.display = 'block';
+  });
+  basket.addEventListener('mouseout', function() {
+    basketDetails.style.opacity = 0;
+    basketDetails.style.display = 'none';
+  });
+  basketDetails.addEventListener('mouseout', function() {
+    basketDetails.style.opacity = 0;
+    basketDetails.style.display = 'none';
+  });
+}
